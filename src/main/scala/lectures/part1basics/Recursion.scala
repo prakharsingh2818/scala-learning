2f198eaed0f74e7a8ecf5c7a3467ed74827bfb12
prakharsingh2818 <prakhar.singh@knoldus.com>
2022-01-10 19:21:21 +0530

    Recursion Call By Name and Value

--- src/main/scala/lectures/part1basics/Recursion.scala
+++ src/main/scala/lectures/part1basics/Recursion.scala
@@ -1,5 +1,7 @@
 package lectures.part1basics
 
+import scala.annotation.tailrec
+
 object Recursion extends App {
 
   def factorial(number: Int): Int = {
@@ -33,6 +35,7 @@ object Recursion extends App {
 */
 
   def tailRecursionFactorial(number: Int): BigInt = {
+    @tailrec
     def calculateFactorial(number: Int, accumulator: BigInt): BigInt = {
       if(number <= 1) accumulator
       else calculateFactorial(number - 1, number*accumulator)
@@ -43,6 +46,7 @@ object Recursion extends App {
   println(tailRecursionFactorial(10000))
 
   def stringConcatenator(message: String, count: Int): String = {
+    @tailrec
     def concatenator(message: String, accumulator: String, count: Int): String = {
       if(count < 1) accumulator
       else concatenator(message, accumulator.concat(message), count - 1)
@@ -52,12 +56,22 @@ object Recursion extends App {
   println(stringConcatenator("Prakhar", 3))
 
   def isPrimeTailRecursion(number: Int): Boolean = {
+    @tailrec
     def isPrime(num: Int, result: Boolean): Boolean = {
       if(num <= 1) result
       else isPrime(num - 1, number%num != 0 && result)
     }
-    isPrime(number/2, true)
+    isPrime(number/2, result = true)
   }
   println(isPrimeTailRecursion(13))
 
+  def fibonacciTailRecursion(number: Int): Int = {
+    @tailrec
+    def calculateFibonacci(num: Int, last: Int, nextToLast: Int): Int = {
+      if(num >= number) last
+      else calculateFibonacci(num + 1, last + nextToLast, last)
+    }
+    if(number <= 2) 1
+    else calculateFibonacci(2, 1, 1)
+  }
 }
